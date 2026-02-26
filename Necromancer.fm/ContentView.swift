f210b22aea8d1531622154be3029611949365604
Kevin Bonilla <82667162+Kevin-Bonilla@users.noreply.github.com>
2026-02-26 17:20:43 -0600

    Initial Commit

---
+++ Necromancer.fm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Necromancer.fm
+//
+//  Created by Kevin Bonilla on 2/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Necromancer.fm/Necromancer_fmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Necromancer_fmApp.swift
+//  Necromancer.fm
+//
+//  Created by Kevin Bonilla on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct Necromancer_fmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
