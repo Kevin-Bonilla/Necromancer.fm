e1695421c0338f67d0f3b32d8b41d034978ecdba
Kevin Bonilla <82667162+Kevin-Bonilla@users.noreply.github.com>
2026-02-27 11:55:43 -0600

    Moving Settings button to the right side

--- Necromancer.fm/Views/MainView.swift
+++ Necromancer.fm/Views/MainView.swift
@@ -14,6 +14,20 @@ struct MainView: View {
         VStack(spacing: 0) {
             // Top banner
             HStack {
+                // Left-side placeholder if you want symmetry or another button
+                Color.clear
+                    .frame(width: 34, height: 34) // matches the size of the settings button
+
+                Spacer()
+
+                // TODO: maybe change this name out for a logo or style it somehow
+                // app name in header
+                Text("Necromancer.fm")
+                    .font(.headline)
+                    .foregroundStyle(.white)
+
+                Spacer()
+                
                 // settings button
                 Button {
                     // boolean that makes the settings show up
@@ -26,20 +40,6 @@ struct MainView: View {
                         .background(.ultraThinMaterial, in: Circle())
                 }
                 .accessibilityLabel("Settings")
-
-                Spacer()
-
-                // TODO: maybe change this name out for a logo or style it somehow
-                // app name in header
-                Text("Necromancer.fm")
-                    .font(.headline)
-                    .foregroundStyle(.white)
-
-                Spacer()
-
-                // Right-side placeholder if you want symmetry or another button
-                Color.clear
-                    .frame(width: 34, height: 34) // matches the size of the settings button
             }
             .padding(.horizontal)
             .padding(.vertical, 10)
@@ -53,13 +53,13 @@ struct MainView: View {
 
             // Main content
             ZStack {
-                Color(.systemBackground)
+                Color(Color.black.opacity(0.9))
                     .ignoresSafeArea(edges: .bottom)
 
-                Text("Main content")
             }
         }
         .sheet(isPresented: $showingSettings) {
+            // when button is pressed for settings show settings view in a sheet
             SettingsView()
         }
     }
