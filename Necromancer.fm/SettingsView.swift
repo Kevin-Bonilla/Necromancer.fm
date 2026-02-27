e2f4775aae06a4aed3c8155f17740f864b7c8db5
Kevin Bonilla <82667162+Kevin-Bonilla@users.noreply.github.com>
2026-02-26 21:28:51 -0600

    Creating Settings List and making outline for linking accounts

--- Necromancer.fm/SettingsView.swift
+++ Necromancer.fm/SettingsView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State var isSpotifyLinked: Bool = false;
+    @State var isAppleMusicLinked: Bool = false;
+    @State var isLastfmLinked: Bool = false;
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section(header: Text("General")) {
+                
+            }
+            
+            Section(header: Text("Accounts")) {
+                // TODO: prob need to change the images for when an account is linked vs unlinked
+                // TODO: make some functionality here to link accounts when clicked
+                
+                Label(isSpotifyLinked ? "Spotify Account" : "Link Spotify", systemImage: "arrow.2.circlepath.circle")
+                
+                Label(isLastfmLinked ? "Last.fm Account" : "Link Last.fm", systemImage: "arrow.2.circlepath.circle")
+                
+                Label(isAppleMusicLinked ? "Apple Music Account" : "Link Apple Music", systemImage: "arrow.2.circlepath.circle")
+            }
+            
+            Section("About") {
+                Label("Version", systemImage: "info.circle")
+            }
+        }
     }
 }
 
