9f61b5432e3619f2a0395c6122e999f59fda3c47
Kevin Bonilla <82667162+Kevin-Bonilla@users.noreply.github.com>
2026-03-02 19:18:51 -0600

    Added a git ignore and LastfmDataService

---
+++ Necromancer.fm/LastfmDataService.swift
@@ -0,0 +1,17 @@
+//
+//  LastfmDataService.swift
+//  Necromancer.fm
+//
+// service where we are going to call the Lastfm API to get the users artist info.
+//
+//  Created by Kevin Bonilla on 3/2/26.
+//
+
+import Foundation
+
+struct LastfmDataService {
+    
+    func getUsersArtistInfo(){
+        
+    }
+}
