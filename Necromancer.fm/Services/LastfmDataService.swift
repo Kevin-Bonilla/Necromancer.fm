548240aadb419fdf0f83d754c3c6a2db91d7f6bb
Kevin Bonilla <82667162+Kevin-Bonilla@users.noreply.github.com>
2026-03-05 00:00:06 -0600

    created helper and service folders and expanded upon lastfm api call func

---
+++ Necromancer.fm/Services/LastfmDataService.swift
@@ -0,0 +1,51 @@
+//
+//  LastfmDataService.swift
+//  Necromancer.fm
+//
+// service where we are going to call the Lastfm API to get the users artist info.
+//
+//  Created by Kevin Bonilla on 3/2/26.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case badURL
+    case requestFailed(Int)
+    case decodingFailed(Error)
+}
+
+struct LastfmDataService {
+    private let lastfmAPIKey: String
+    private let lastfmAPIurl: String = "https://ws.audioscrobbler.com/2.0/"
+    
+    init(lastfmAPIKey: String = Secrets.lastfmAPIKey) {
+        // init api key on creation
+        self.lastfmAPIKey = lastfmAPIKey
+    }
+    
+    func getUsersArtistInfo() async throws {
+        // build url for our api request
+        var urlComponents = URLComponents(string: lastfmAPIurl)!
+        urlComponents.queryItems = [
+            URLQueryItem(name: "method", value: "user.gettopartists"),
+            URLQueryItem(name: "user", value: "x762-"), // TODO: unhardcode this user later
+            URLQueryItem(name: "api_key", value: lastfmAPIKey),
+            URLQueryItem(name: "format", value: "json")
+        ]
+            
+        guard let url = urlComponents.url else {
+            throw NetworkError.badURL
+        }
+        
+        // requesting data
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        // throwing error when bad response
+        if let http = response as? HTTPURLResponse, !(200...299).contains(http.statusCode) {
+            throw NetworkError.requestFailed(http.statusCode)
+        }
+        // TODO: decode `data` into your expected model type
+        
+    }
+}
